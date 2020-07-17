804aaf9a533f571285c12f57aac4b5ea305b76bb
Christian Segura <ces1508@gmail.com>
2020-07-17 16:10:18 -0500

    Updates

---
+++ precache-manifest.926cb251bf440456a6956470e3f675f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea00a2b99252b7c987dee9b3553b0f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8636dad58800e44d6fb",
+    "url": "/static/css/main.0d49c537.chunk.css"
+  },
+  {
+    "revision": "5d6a15b4d301d58eae4e",
+    "url": "/static/js/2.11d74aad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.11d74aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8636dad58800e44d6fb",
+    "url": "/static/js/main.e95bf4bf.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
