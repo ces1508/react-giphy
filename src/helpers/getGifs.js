6ce2d3342ba56e08a346a8349e5670e8b66790c7
Christian Segura <ces1508@gmail.com>
2020-07-17 17:30:42 -0500

    test helper getGifts

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,4 +1,7 @@
 const getGifs = async category => {
+
+  if (!category) throw new Error('you need send a category')
+
   const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=mQyfl0YYKW18ivTdrxlpqZIGWvHIx1Wt`
   const res = await fetch(url)
   const { data } = await res.json()
---
+++ src/tests/components/helpers/getGifs.test.js
@@ -0,0 +1,12 @@
+import getGifs from '../../../helpers/getGifs'
+
+describe('test on helper/getGifts', () => {
+  it ('should be return 10 images', async () => {
+    const images = await getGifs('one puch man')
+    expect(images.length).toBe(10)
+  })
+
+  it ('should be fail if dont send a category', async () => {
+    await expect(getGifs()).rejects.toThrow()
+  })
+})
